84c2b60392c72b2cdb182f05b7f7b00c269b0e15
darshan <darsh2605@gmail.com>
2023-11-27 01:25:56 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/darshankomu/match_cards/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.darshankomu.match_cards
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/HomeScreens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreens extends StatefulWidget {
+  const HomeScreens({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreens> createState() => _HomeScreensState();
+}
+
+class _HomeScreensState extends State<HomeScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
